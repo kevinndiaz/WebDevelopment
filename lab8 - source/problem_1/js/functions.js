f99f981113b94c01d49ddb4325dae0059c55374e
Kevin Diaz <diazk2@wit.edu>
2022-03-19 01:29:46 -0400

    problem 1 solved

--- lab8 - source/problem_1/js/functions.js
+++ lab8 - source/problem_1/js/functions.js
@@ -4,14 +4,40 @@ function calculateTotal(quantity, price){
     return quantity * price;
 }
 
-function outputCartRow(item, total){
+function outputCartRow(image, item, quantity, price, total){
+    document.write(
+        "<tr>\n" +
+        "<td><img src=images/" + image + "></td>\n" +
+        "<td>" + item + "</td>\n" +
+        "<td>" + quantity + "</td>\n" +
+        "<td>$" + price.toFixed(2) + "</td>\n" +
+        "<td>$" + total.toFixed(2) + "</td>\n" +
+        "</tr>\n"
+    );
+}
+
+function outputReceipt(){
     document.write(
         "<tr class='totals'>\n" +
-        "<td colspan='4' class='focus'>" + item + "</td>\n" +
-        "<td class='focus'>$" + total + "</td>\n" +
+        "<td colspan='4'>Subtotal</td>\n" +
+        "<td>$" + subTotal.toFixed(2) + "</td>\n" +
+        "</tr>\n" +
+
+        "<tr class='totals'>\n" +
+        "<td colspan='4'>Tax</td>\n" +
+        "<td>$" + parseInt(tax_rate).toFixed(2) + "</td>\n" +
+        "</tr>\n" +
+
+        "<tr class='totals'>\n" +
+        "<td colspan='4'>Shipping</td>\n" +
+        "<td>$" + parseInt(shipping_threshold).toFixed(2) + "</td>\n" +
+        "</tr>\n" +
+
+        "<tr class='totals'>\n" +
+        "<td colspan='4'>Grand Total</td>\n" +
+        "<td class='focus'>$" + grandTotal.toFixed(2) + "</td>\n" +
         "</tr>\n"
-        
-        );
+    );
 }
 
 
--- lab8 - source/problem_1/js/lab8_problem1.js
+++ lab8 - source/problem_1/js/lab8_problem1.js
@@ -5,7 +5,15 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
 
-   for(let i = 0; i < cart.length; i++){
-      let total = calculateTotal(cart[i].quantity, cart[i].product.price);
-      outputCartRow(cart[i].product.title, total);
-   }
+let subTotal = 0;
+let grandTotal = 0;
+
+for(let i = 0; i < cart.length; i++){
+   let total = calculateTotal(cart[i].quantity, cart[i].product.price);
+   outputCartRow(cart[i].product.filename, cart[i].product.title, cart[i].quantity, cart[i].product.price, total);
+   subTotal += total
+}
+
+grandTotal = subTotal + parseInt(tax_rate) + parseInt(shipping_threshold);
+
+outputReceipt();
